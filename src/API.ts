cd4bf53ad23783a6b91e541e2d8ae939cc305d41
Sandali <sandali.s@hsenidmobile.com>
2023-08-24 10:13:31 +0530

    corrected the endpoint

--- src/API.ts
+++ src/API.ts
@@ -5,7 +5,7 @@ export enum Difficulty{
 }
 
 export const fetchQuestions =async (amount:number,difficulty: Difficulty) => {
-    const endpoint = 'https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple'
+    const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`
     const data = await (await fetch(endpoint)).json();
     console.log(data);
 }
